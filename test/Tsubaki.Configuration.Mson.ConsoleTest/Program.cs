1eb8e25e6987beaad9c8895dd4f0863114ff37f8
0x0001F36D <hanabiranoshitaniumore@gmail.com>
2018-11-01 10:30:34 +0800

    加入專案檔案。

---
+++ test/Tsubaki.Configuration.Mson.ConsoleTest/Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Tsubaki.Configuration.Mson.ConsoleTest
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
